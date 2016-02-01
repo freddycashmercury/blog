4b89f5b82797376640cace20643925093974e647
Cody Works <codyaworks@gmail.com>
2016-01-31 19:01:17 -0600

    Add user model and basic actions.
    Add user model with bcrypt auth, add styling, add login form for user
    and user show page. Add welcome page. Add controllers for static pages
    and user.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Account created"
+      render 'show'
+    else 
+      flash[:notice] = "Invalid account information"
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(user_params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :username, :password, :password_confirmation, :email) 
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  attr_accessor :username, :email 
+  has_secure_password
 end
